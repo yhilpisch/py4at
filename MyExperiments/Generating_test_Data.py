689329cef257a1aa03aa7cd99372b533a60485cb
Gianni <dreck@posteo.de>
2022-02-14 16:42:00 -0500

    3comma Signals now prepared for backtesting

---
+++ MyExperiments/Generating_test_Data.py
@@ -0,0 +1,50 @@
+from random import random
+
+import numpy
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def generate_simpel_sample_slope(rows=100, freq='1min') -> pd.DataFrame:
+    rows = int(rows)
+    # generate a DatetimeIndex object given the frequency
+    index = pd.date_range('2021-1-1', periods=rows, freq=freq)
+    row = np.linspace(rows, 1, num=rows)
+    # generate the DataFrame object
+    df = pd.DataFrame({"open": row, "buy": np.nan}, index=index)
+    df["buy"][0] = 1
+    return df
+
+
+def add_candle_data(df):
+    df["high"] = df["open"] + 0.5
+    df["low"] = df["open"] - 0.5
+    df["close"] = df["open"]
+    df["volume"] = 100
+
+def generate_simpel_sample_momentum(rows=100, freq='1min') -> pd.DataFrame:
+    rows = int(rows)
+    # generate a DatetimeIndex object given the frequency
+    index = pd.date_range('2021-1-1', periods=rows * 2, freq=freq)
+    row = np.append(np.linspace(rows, 1, num=rows), np.linspace(1, rows, num=rows))
+    # generate the DataFrame object
+    df = pd.DataFrame({"open": row, "buy_signal": np.nan}, index=index)
+    add_candle_data(df)
+    df["buy_signal"][0] = 1
+    return df
+
+
+# test:
+hshsh = generate_simpel_sample_momentum()
+print("test")
+
+def get_data_from_file(start, end):
+    ''' Retrieves and prepares the row_data.
+    '''
+    raw = pd.read_csv(
+        '/Users/hans/Documents/Github/GiannisTraidingReserching/MyExperiments/historyCryptoData/Binance_BTCUSDT_minute.csv',
+        index_col=1,  # ist wichtig Index muss Datum sein sonst geht raw.loc[ nicht
+        parse_dates=True).dropna().drop(columns="unix")
+    raw = raw.loc[start:end]
+    # raw.rename(columns={self.symbol: 'price'}, inplace=True)
+    return raw
