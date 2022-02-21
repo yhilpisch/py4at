e8846d69b23f387aad6b80ded624fb334f29d71d
Gianni <dreck@posteo.de>
2022-02-20 23:37:04 -0500

    3comma -created auto docker creater and destroyer for digitalOcean

--- Docker/Run_docker.py
+++ Docker/Run_docker.py
@@ -1,23 +1,59 @@
-from config import config
-from digitalOcean.Run_Droplet import Droplet
+import sys
+import click as click
+from jsonpickle import json
+from digitalOcean import config
+from digitalOcean.Droplet import Droplet
 import subprocess
+import docker
+
 
 
 # https://www.digitalocean.com/community/tutorials/how-to-use-a-remote-docker-server-to-speed-up-your-workflow
 def set_up_droplet_server():
-    droplet = Droplet()
+    new_droplet = Droplet()
+    new_droplet.creating_a_new_droplet_with_all_your_SSH_keys()
+    host = new_droplet.droplet.ip_address  # '143.198.239.46'
     # droplet.Creating_a_new_droplet_with_all_your_SSH_keys()
+    return new_droplet
 
+
+def create_user(server):
     #
     user = "root"
-    host = droplet.droplet.ip_address # '143.198.239.46'
+    host = server.droplet.ip_address  # '143.198.239.46'
     command = ['sudo useradd -m -d /home/dockergianni -s /bin/bash dockergianni',
-               'sshpass -p {password} ssh {user}@{host}'.format(user=user, host=host, password=config.usr_dockergianni_pw)]
+               'sshpass -p {password} ssh {user}@{host}'.format(user=user, host=host,
+                                                                password=config.usr_dockergianni_pw)]
     for cmd in command:
         result = subprocess.Popen("ssh {user}@{host} {cmd}".format(user=user, host=host, cmd=cmd), shell=True,
                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
         print(result)
 
 
+def shutdown_all_droplets(manager):
+    my_droplets = manager.get_all_droplets()
+    for droplet in my_droplets:
+        droplet.shutdown()
+
+
+def build_docker(drop):
+    ssh_client = docker.DockerClient(base_url="ssh://root@{host}".format(host=drop.ip_address))
+    #print(ssh_client.containers.list())
+    image, build_logs = ssh_client.images.build(path=sys.path[2], tag='backtest', rm=False)  # Error by: , tag='BTC_BackTest_long_py', rm=False
+    for line in build_logs:
+        print(line)
+
+    container = ssh_client.containers.run("backtest", detach=True)
+    for line in container.logs(stream=True):
+        print(line.strip())
+    print("dd")
+
+
 if __name__ == '__main__':
-    set_up_droplet_server()
+    drop = set_up_droplet_server()
+    #drop =  Droplet.get_all_droplets()[0]
+    #host = drop.ip_address
+    #build_docker(drop)
+    #create_user(drop)
+    print("ee")
+
--- digitalOcean/Droplet.py
+++ digitalOcean/Droplet.py
@@ -1,27 +1,28 @@
 import digitalocean
 from digitalocean import SSHKey
-from MyExperiments.config import config
+from digitalOcean import config
 
 
 class Droplet(object):
-    def __init__(self):
-        self.manager = digitalocean.Manager(token=config.digitalocean_token)
-        self.droplets = self.manager.get_all_droplets()
-        self.droplet = self.droplets[0]
+    droplets = None
+    manager = digitalocean.Manager(token=config.digitalocean_token)
 
-    def Creating_a_new_droplet_with_all_your_SSH_keys(self):
+    def __init__(self):
+        self.droplet = None
+
+    def creating_a_new_droplet_with_all_your_SSH_keys(self):
         keys = self.manager.get_all_sshkeys()
         self.droplet = digitalocean.Droplet(token=self.manager.token,
                                             name='BTC-oBT-long',
                                             region='sfo3',  # https://slugs.do-api.dev
-                                            image='rancheros',  # https://slugs.do-api.dev
+                                            image='docker-20-04',  # https://slugs.do-api.dev
                                             size_slug='s-1vcpu-1gb',  # https://slugs.do-api.dev
                                             ssh_keys=keys,
                                             backups=False)
         m = self.droplet.create()
         print(m)
 
-    def Add_SSHKey_into_DigitalOcean_Account(self):
+    def add_SSHKey_into_DigitalOcean_Account(self):
         user_ssh_key = open('/Users/hans/.ssh/public_lette.pub').read()
         key = SSHKey(token=self.manager.token,
                      name=self.droplet.name,
@@ -29,7 +30,7 @@ class Droplet(object):
         m = key.create()
         print(m)
 
-    def Checking_the_status_of_the_droplet(self):
+    def checking_the_status_of_the_droplet(self):
         if self.droplet is None:
             my_droplets = self.manager.get_all_droplets()
             self.droplet = self.droplet
@@ -39,9 +40,18 @@ class Droplet(object):
             # Once it shows "completed", droplet is up and running
             print(action.status)
 
+    def destroy_droplets(self):
+        self.droplet.shutdown()  # cos still costs if it just turned off
+        if not self.droplet.destroy():
+            raise InterruptedError("Could not destroy Server")
 
-if __name__ == '__main__':
-    droplet = Droplet()
-    droplet.Add_SSHKey_into_DigitalOcean_Account()
-    #droplet.Creating_a_new_droplet_with_all_your_SSH_keys()
-    #droplet.Checking_the_status_of_the_droplet()
+    @staticmethod
+    def get_all_droplets():
+        Droplet.droplets = Droplet.manager.get_all_droplets()
+        return Droplet.droplets
+
+# if __name__ == '__main__':
+# droplet = Droplet()
+# droplet.add_SSHKey_into_DigitalOcean_Account()
+# droplet.creating_a_new_droplet_with_all_your_SSH_keys()
+# droplet.checking_the_status_of_the_droplet()
