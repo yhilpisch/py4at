393c291d09fba1bc1b62a9449a0b2a72077a7834
Gianni <dreck@posteo.de>
2022-02-26 20:20:12 -0500

    3comma - print telegram from python

--- MyExperiments/config/run.sh
+++ MyExperiments/config/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#telegram-send -g "Run Run_backtest.py"
-#cd /runfolder || exit
-# OUTPUT=$(python main_run.py); telegram-send -g "${OUTPUT}"
-
-stdbuf -oL python script.py > OUTPUT
-
-echo logout
+telegram-send -g "Run Run_backtest.py"
+cd /runfolder || exit
+OUTPUT=$(python main_run.py); telegram-send -g "${OUTPUT}"
